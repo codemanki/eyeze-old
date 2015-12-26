1b9b7f840e5511ab1aae552bbb22fe94a4970764
Oleksii Sribnyi <me@codemanki.com>
2015-12-26 13:24:34 +0100

    Basic riotjs integration

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,11 +2,18 @@
 'use strict';
 
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   module: {
+    preLoaders: [{
+      test: /\.tag$/,
+      exclude: /node_modules/,
+      loader: 'riotjs-loader',
+      query: { type: 'none' }
+    }],
     loaders: [{
-      test: /\.jsx?$/,
+      test: /\.js|\.tag$/,
       loaders: ['babel-loader'],
       exclude: /node_modules/
     }]
@@ -21,7 +28,9 @@ module.exports = {
     packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
   },
   plugins: [
-
+   new webpack.ProvidePlugin({
+      riot: 'riot'
+    })
   ],
   externals: [
     // put your node 3rd party libraries which can't be built with webpack here (mysql, mongodb, and so on..)
